e1ec985b1f7fbd8671035714cf677b4672fe5c93
AshClasse <classeashley@gmail.com>
2024-07-23 22:57:45 -0400

    nullable warnings fixed

--- RealEstateApplication.Domain/Settings/JWTSettings.cs
+++ RealEstateApplication.Domain/Settings/JWTSettings.cs
@@ -2,9 +2,9 @@
 {
     public class JWTSettings
     { 
-        public string Key { get; set; }
-        public string Issuer { get; set; }
-        public string Audience { get; set; }
+        public string? Key { get; set; }
+        public string? Issuer { get; set; }
+        public string? Audience { get; set; }
         public int DurationInMinutes { get; set; }
     }
 }
